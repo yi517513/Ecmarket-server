b9c69e1406ac60daac43ec332002afa5d66b9861
YI <yi517513@gmail.com>
2025-06-23 21:31:49 +0800

    update

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,6 +5,7 @@ const { requireAuth } = require("../middlewares/auth");
 // 接收金流 api 回傳結果
 router.post("/result", paymentController.handlePaymentCallback);
 
+// 取得付款 html
 router.get("/:orderId", requireAuth, paymentController.getPaymentHtml);
 
 module.exports = router;
